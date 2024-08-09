916c48966fbe9537be84ef204555a84cabca8dd5
truittjanney <truittjanney93@gmail.com>
2024-08-09 10:50:15 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my-first-gatsby-site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
